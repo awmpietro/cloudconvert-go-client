2ab530b0873c0d8562b7cb32ec91d643b0f2c000
Arthur <awmpietro@hotmail.com>
2017-08-12 00:58:44 -0300

    Packaging

--- convert.go
+++ convert.go
@@ -1,30 +1,15 @@
-package main
+package cloudconvgocl
 
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 	"os"
 )
 
-//Boot
-func Boot(data map[string]string) {
+func Convert(data map[string]string) error {
 	processUrl := "https://api.cloudconvert.com/process"
-	//Example:
-	data = make(map[string]string)
-	data["key"] = ""
-	data["from"] = ""
-	data["to"] = ""
-	data["filePath"] = ""
-	data["filePath"] = ""
-	data["fileName"] = ""
-	data["outputFmt"] = ""
-
-	data["input"] = ""
-	data["mode"] = ""
-	data["pathToSave"] = ""
 	if data["input"] == "" {
 		data["input"] = "download"
 	}
@@ -43,28 +28,27 @@ func Boot(data map[string]string) {
 	}
 	processJSON, err := json.Marshal(process)
 	if err != nil {
-		fmt.Println("Erro: ", err)
+		return err
 	}
 
 	resp, err := http.Post(processUrl, "application/json", bytes.NewReader(processJSON))
 
 	if err != nil {
-		fmt.Println("Erro: ", err)
+		return err
 	}
 	defer resp.Body.Close()
-	//Unmarshall json to Go Struct
 	var processResponse ProcessResponse
 	err = json.NewDecoder(resp.Body).Decode(&processResponse)
 	if err != nil {
-		fmt.Println("Erro: ", err)
+		return err
 	}
 
-	processStart(processResponse, data)
+	err = processStart(processResponse, data)
+	return err
 
 }
 
-//ProcessStart
-func processStart(processResponse ProcessResponse, data map[string]string) {
+func processStart(processResponse ProcessResponse, data map[string]string) error {
 	processStart := ProcessStart{
 		Wait:         true,
 		Input:        data["input"],
@@ -74,40 +58,36 @@ func processStart(processResponse ProcessResponse, data map[string]string) {
 	}
 	processJSONStart, err := json.Marshal(processStart)
 	if err != nil {
-		fmt.Println("Erro: ", err)
+		return err
 	}
 	resp, err := http.Post("https:"+processResponse.Url, "application/json", bytes.NewReader(processJSONStart))
 
 	if err != nil {
-		fmt.Println("Erro: ", err)
+		return err
 	}
 
-	//Unmarshall json to Go Struct
 	var processStartResponse ProcessStartResponse
 	err = json.NewDecoder(resp.Body).Decode(&processStartResponse)
 	if err != nil {
-		fmt.Println("Erro: ", err)
+		return err
 	}
 
-	fileDownload(processStartResponse, data)
+	err = fileDownload(processStartResponse, data)
+	return err
 }
 
-//fileDownload
 func fileDownload(processStartResponse ProcessStartResponse, data map[string]string) error {
 	out, err := os.Create(data["pathToSave"] + processStartResponse.Output.Filename)
 	if err != nil {
-		fmt.Println("Erro: ", err)
 		return err
 	}
 	defer out.Close()
 	resp, err := http.Get("https:" + processStartResponse.Output.Url)
 	if err != nil {
-		fmt.Println("Erro: ", err)
 		return err
 	}
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
-		fmt.Println("Erro: ", err)
 		return err
 	}
 	return nil
--- model.go
+++ model.go
@@ -1,4 +1,4 @@
-package main
+package cloudconvgocl
 
 type Data struct {
 	Key               string `toml:"key"`
